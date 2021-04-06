d1d5dd0271e5641c97b788fd18edf9f47b5cfe1a
Tanay Raj <tanay.raj76@gmail.com>
2021-04-06 18:01:43 +0530

    adding state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import classes from './Burger.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
 const burger = (props)=>{
 
+    const transformedIngredients = Object.keys()
+
     return(
-        <div>
+        <div className={classes.Burger}>
         
+        <BurgerIngredient type="bread-top"/>
+        <BurgerIngredient type="cheese"/>
+        <BurgerIngredient type="meat"/>
+        <BurgerIngredient type="bread-bottom"/>
         
         </div>
     );
