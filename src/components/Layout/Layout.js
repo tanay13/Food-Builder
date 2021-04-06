9d617501ce210bde3205eeab1d4982af2e71d8c2
Tanay Raj <tanay.raj76@gmail.com>
2021-04-06 17:36:20 +0530

    components add

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Aux from '../../hoc/Auxi' 
+import classes from './Layout.css'
+const layout = (props)=>(
+
+    <Aux>
+        <div> Toolbar, sideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        
+        </main>
+    </Aux>
+)
+
+export default layout
+  
+        
+    
+   
